3aa08bfe9416bd64a8f56e9484b6d37f6b2a6fe7
André <andrejusmattos@gmail.com>
2024-04-12 09:29:38 -0300

    Bind do nome do usuário

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { UserService } from '../../services/users-storage.service';
 
 
 @Component({
@@ -15,6 +16,12 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
+  loggedUser: any;
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.loggedUser = this.userService.getLoggedUser();
+  }
 }
