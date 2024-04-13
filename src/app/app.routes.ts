8780a786bd5d86dc761392ec4f8cc53d5d86d352
André <andrejusmattos@gmail.com>
2024-04-13 08:22:42 -0300

    FormGroup do cadastro de pacientes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { CardInfoPacientesComponent } from './components/home/card-info-pacientes/card-info-pacientes.component';
+import { CadastroPacientesComponent } from './components/cadastro-pacientes/cadastro-pacientes.component';
 
 
 export const routes: Routes = [
   { path: '', component: LoginComponent},
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'card-paciente', component: CardInfoPacientesComponent}
+  { path: 'cadastro-paciente', component: CadastroPacientesComponent}
 ];
---
+++ src/app/components/cadastro-pacientes/cadastro-pacientes.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-cadastro-pacientes',
+  standalone: true,
+  imports: [
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './cadastro-pacientes.component.html',
+  styleUrl: './cadastro-pacientes.component.scss'
+})
+export class CadastroPacientesComponent {
+
+fullName: string | undefined;
+gender: string | undefined;
+email: string | undefined;
+
+patRegForm = new FormGroup ({
+  fullName: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(64)]),
+  gender: new FormControl('', [Validators.required]),
+  dataNascimento: new FormControl('', [Validators.required]),
+  cpf: new FormControl('', [Validators.required, Validators.pattern(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)]),
+  rg: new FormControl('', [Validators.required, Validators.maxLength(20)]),
+  estadoCivil: new FormControl('', [Validators.required]),
+  telefone: new FormControl('', [Validators.required, Validators.pattern(/^\(\d{2}\) \d \d{4}-\d{5}$/)]),
+  email: new FormControl('', [Validators.email]),
+  naturalidade: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(64)]),
+  contatoEmergencia: new FormControl('', [Validators.required, Validators.pattern(/^\(\d{2}\) \d \d{4}-\d{5}$/)]),
+  alergias: new FormControl(''),
+  cuidadosEspecificos: new FormControl(''),
+  convenio: new FormControl(''),
+  numeroConvenio: new FormControl(''),
+  validadeConvenio: new FormControl(''),
+  cep: new FormControl(''),
+  cidade: new FormControl(''),
+  estado: new FormControl(''),
+  logradouro: new FormControl(''),
+  numero: new FormControl(''),
+  complemento: new FormControl(''),
+  bairro: new FormControl(''),
+  pontoReferencia: new FormControl('')
+});
+
+
+
+  submitForm(){
+
+  }
+}
