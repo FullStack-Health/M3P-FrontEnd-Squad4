b3afca6d414aa3e654dcfb91b5a191d4f3a74cb1
André <andrejusmattos@gmail.com>
2024-10-23 20:58:38 -0300

    Configuração de máscara da senha

--- src/app/components/listagem-usuarios/listagem-usuarios.component.ts
+++ src/app/components/listagem-usuarios/listagem-usuarios.component.ts
@@ -30,7 +30,7 @@ import { User } from '../../entities/user.model';
 export class ListagemUsuariosComponent implements OnInit {
 
   usersList: User[] = [];
-  displayedColumns: string[] = ['id', 'email', 'password', 'acao'];
+  displayedColumns: string[] = ['id', 'email', 'senhaComMascara', 'acao'];
   textoPesquisa: any;
 
   constructor(
