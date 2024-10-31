8c818b56ac5e4988b227cb61bfab0a3240686e45
André <andrejusmattos@gmail.com>
2024-10-31 19:38:08 -0300

    Implementação de prontuário de paciente

--- src/app/pipes/formatar-telefone.pipe.ts
+++ src/app/pipes/formatar-telefone.pipe.ts
@@ -6,6 +6,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormatarTelefonePipe implements PipeTransform {
   transform(telefone: string): string {
-    return `(${telefone.substring(0, 2)}) ${telefone.substring(2, 3)} ${telefone.substring(3, 7)}-${telefone.substring(7)}`;
+    if (telefone.length === 11) {
+      // Formato para telefones com 11 dígitos (xx)xxxxx-xxxx
+      return `(${telefone.substring(0, 2)})${telefone.substring(2, 7)}-${telefone.substring(7)}`;
+    } else if (telefone.length === 10) {
+      // Formato para telefones com 10 dígitos (xx)xxxx-xxxx
+      return `(${telefone.substring(0, 2)})${telefone.substring(2, 6)}-${telefone.substring(6)}`;
+    } else {
+      // Retorna o telefone sem formatação se não tiver 10 ou 11 dígitos
+      return telefone;
+    }
   }
 }
