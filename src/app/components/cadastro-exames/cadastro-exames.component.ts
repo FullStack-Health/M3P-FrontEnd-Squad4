188880e98b922ef7a9caf734d26be62369ef3508
André <andrejusmattos@gmail.com>
2024-11-01 13:49:16 -0300

    Listagem e busca de pacientes em cadastro-exames

--- src/app/components/cadastro-exames/cadastro-exames.component.ts
+++ src/app/components/cadastro-exames/cadastro-exames.component.ts
@@ -15,6 +15,7 @@ import { ExamesService } from '../../services/exames.service';
 import { MatDatepicker, MatDatepickerModule } from '@angular/material/datepicker';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Paciente } from '../../entities/paciente.model';
 
 @Component({
   selector: 'app-cadastro-exames',
@@ -45,12 +46,12 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   ]
 })
 export class CadastroExamesComponent implements OnInit {
-  pacientes: any[] = [];
-  textoPesquisa: string = '';
-  pacienteSelecionado: { id: string; nomeCompleto: string } | null = null;
+  pacientes: Paciente[] = [];
+  pacienteSelecionado: { id: string; nome: string } | null = null;
   displayedColumns: string[] = ['registro', 'nomePaciente', 'acao'];
   exameId: string | any;
   exameForm: FormGroup;
+  textoPesquisa: string = '';
 
   constructor (
     private readonly pageTitleService: PageTitleService,
@@ -63,7 +64,7 @@ export class CadastroExamesComponent implements OnInit {
     this.pageTitleService.setPageTitle('CADASTRO DE EXAMES');
 
     this.exameForm = new FormGroup({
-      nomeCompletoPaciente: new FormControl(''),
+      nome: new FormControl(''),
       idPaciente: new FormControl(''),
       nomeExame: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(64)]),
       dataExame: new FormControl('', [Validators.required]),
@@ -90,30 +91,33 @@ export class CadastroExamesComponent implements OnInit {
     const exame = this.examesService.getExamePorId(this.exameId);
     if (exame) {
       this.exameForm.patchValue(exame);
-      this.exameForm.get('nomeCompletoPaciente')?.disable();
+      this.exameForm.get('nome')?.disable();
     } else {
       console.error('Exame não encontrado');
     }
   }
 
   atualizarListaPacientes() {
-    this.pacientes = this.pacientesService.obterPacientes();
+    this.pacientesService.getPacientes().subscribe((pacientes: Paciente[]) => {
+      this.pacientes = pacientes;
+    });
   }
 
-  pesquisarPacientes() {
-    const textoPesquisa = this.textoPesquisa.trim();
-    if (!textoPesquisa) {
-      this.atualizarListaPacientes();
-    } else {
-      this.pacientes = this.pacientesService.pesquisarPacientes(textoPesquisa);
-      this.pacienteSelecionado = null;  
-    }
+  pesquisarPacientes(textoPesquisa: any) {
+    // console.log('pesquisarPacientes chamado com:', textoPesquisa);
+    const buscaInput = this.textoPesquisa;
+    // console.log('Texto de pesquisa:', buscaInput);
+    this.pacientesService.getPacientesPorEmailOuPorId(buscaInput).subscribe(pacientes => {
+      // console.log('Pacientes encontrados:', pacientes);
+      this.pacientes = Array.isArray(pacientes) ? pacientes : [pacientes];
+      // console.log('Pacientes atribuídos:', this.pacientes);
+    });
   }
 
   selecionarPaciente(paciente: any) {
     this.pacienteSelecionado = paciente;
     this.exameForm.patchValue({
-      nomeCompletoPaciente: paciente.nomeCompleto,
+      nome: paciente.nome,
       idPaciente: paciente.id
     });
   }
